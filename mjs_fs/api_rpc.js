74b658f3489a080436b59846f0987286fd0b93bd
Бобби <lsm@cesanta.com>
2017-10-20 13:43:28 +0100

    Allow to send RPC errors from JS
    Also optimise the code slightly.
    Add error handling to the example.
    
    PUBLISHED_FROM=4d54f08114bcf2ff5833fefaf2b1149978f3baba

--- mjs_fs/api_rpc.js
+++ mjs_fs/api_rpc.js
@@ -1,12 +1,18 @@
 let RPC = {
-  _strdup: ffi('void *strdup(char *)'),
-  _addHandler: ffi('void *mgos_rpc_add_handler(void *, void (*)(void *, char *, char *, userdata), userdata)'),
-  _sendResponse: ffi('bool mgos_rpc_send_response(void *, char *)'),
+  _sdup: ffi('void *strdup(char *)'),
+  _ah: ffi('void *mgos_rpc_add_handler(void *, void (*)(void *, char *, char *, userdata), userdata)'),
+  _resp: ffi('bool mgos_rpc_send_response(void *, char *)'),
   _call: ffi('bool mgos_rpc_call(char *, char *, char *, void (*)(char *, int, char *, userdata), userdata)'),
+  _err: ffi('bool mg_rpc_send_errorf(void *, int, char *, char *)'),
 
   _ahcb: function(ri, args, src, ud) {
+    // NOTE(lsm): not using `this` here deliberately. Calleth from C.
     let resp = ud.cb(JSON.parse(args || 'null'), src, ud.ud);
-    RPC._sendResponse(ri, JSON.stringify(resp));
+    if (typeof(resp) === 'object' && typeof(resp.error) === 'number') {
+      RPC._err(ri, resp.error, '%s', resp.message || '');
+    } else {
+      RPC._resp(ri, JSON.stringify(resp));
+    }
     // NOTE: we don't call ffi_cb_free here because this handler might be used
     // more than once
   },
@@ -21,29 +27,26 @@ let RPC = {
   // ## **`RPC.addHandler(name, handler)`**
   // Add RPC handler. `name` is a string like `'MyMethod'`, `handler`
   // is a callback function which takes `args` arguments object.
+  // If a handler returns an object with a numeric `error` attribute and
+  // optional `message` string attribute, the caller will get a failure.
   //
   // Return value: none.
   //
   // Example:
   // ```javascript
   // RPC.addHandler('Sum', function(args) {
-  //   return args.a + args.b;
+  //   if (typeof(args) === 'object' && typeof(args.a) === 'number' &&
+  //       typeof(args.b) === 'number') {
+  //     return args.a + args.b;
+  //   } else {
+  //     return {error: -1, message: 'Bad request. Expected: {"a":N1,"b":N2}'};
+  //   }
   // });
-  // ```
-  // The installed handler is available over Serial, Restful, MQTT, Websocket,
-  // for example over Websocket:
-  // ```bash
-  // $ mos --port ws://192.168.0.206/rpc call Sum '{"a":1, "b": 2}'
-  // 3
-  // ```
-  // Or, over familiar RESTful call:
-  // ```bash
-  // $ curl -d '{"a":1, "b": 2}' 192.168.0.206/rpc/Sum
-  // 3
   // ```
   addHandler: function(name, cb, ud) {
     let data = {cb: cb, ud: ud};
-    this._addHandler(this._strdup(name), this._ahcb, data);
+    // TODO(lsm): get rid of this strdup() leak. One-off, but still ugly.
+    this._ah(this._sdup(name), this._ahcb, data);
   },
 
   // ## **`RPC.call(dst, method, args, callback)`**
